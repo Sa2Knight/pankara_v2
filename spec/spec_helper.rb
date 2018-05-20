8eb8fc872f9904916bdc6b5e3b1a4639893bbe91
Shingo Sasaki <shingo.sasaki.0529@gmail.com>
2018-05-20 16:12:09 +0000

    Rspecで失敗したテストのみ再度走らせられるように　”

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,7 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
   config.shared_context_metadata_behavior = :apply_to_host_groups
+  config.example_status_persistence_file_path = 'spec/examples.txt'
 
   config.before(:suite) do
     DatabaseCleaner.strategy = :truncation
