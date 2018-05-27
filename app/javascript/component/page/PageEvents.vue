994be2c7958da952a471c3aaae2c945aeef81a6f
Shingo Sasaki <shingo.sasaki.0529@gmail.com>
2018-05-27 14:24:19 +0000

    カラオケ一覧で自分のカラオケのみ絞り込めるように

--- app/javascript/component/page/PageEvents.vue
+++ app/javascript/component/page/PageEvents.vue
@@ -2,40 +2,54 @@
   <div>
     <div class="title">カラオケ一覧</div>
     <section>
-
+      <b-field>
+        <b-radio-button v-model="search.target"
+          native-value="mine"
+          type="is-success">
+          <span>自分が参加したカラオケ</span>
+        </b-radio-button>
+        <b-radio-button v-model="search.target"
+          native-value="all"
+          type="is-info">
+          <span>全てのカラオケ</span>
+        </b-radio-button>
+      </b-field>
     </section>
-    <b-table
-      narrowed
-      paginated
-      backend-pagination
-      :data="events"
-      :total="pager.total"
-      :per-page="pager.per"
-      :page="pager.page"
-      :loading="isLoading"
-      @page-change="onPageChanged"
-    >
-      <template slot-scope="props">
-        <b-table-column label="タイトル">
-          {{ props.row.title  }}
-        </b-table-column>
-        <b-table-column label="日付">
-          {{ props.row.datetime  }}
-        </b-table-column>
-        <b-table-column label="時間">
-          {{ props.row.plan  }} 時間
-        </b-table-column>
-        <b-table-column label="お店">
-          <v-store-label :store="props.row.store" />
-        </b-table-column>
-        <b-table-column label="曲数">
-          {{ props.row.history_size  }}
-        </b-table-column>
-        <b-table-column label="メンバー">
-          <v-user-icons :users="props.row.members" />
-        </b-table-column>
-      </template>
-    </b-table>
+    <section>
+      <b-table
+        narrowed
+        paginated
+        pagination-simple
+        backend-pagination
+        :data="events"
+        :total="pager.total"
+        :per-page="pager.per"
+        :current-page="pager.page"
+        :loading="isLoading"
+        @page-change="onPageChanged"
+      >
+        <template slot-scope="props">
+          <b-table-column label="タイトル">
+            {{ props.row.title  }}
+          </b-table-column>
+          <b-table-column label="日付">
+            {{ props.row.datetime  }}
+          </b-table-column>
+          <b-table-column label="時間">
+            {{ props.row.plan  }} 時間
+          </b-table-column>
+          <b-table-column label="お店">
+            <v-store-label :store="props.row.store" />
+          </b-table-column>
+          <b-table-column label="曲数">
+            {{ props.row.history_size  }}
+          </b-table-column>
+          <b-table-column label="メンバー">
+            <v-user-icons :users="props.row.members" />
+          </b-table-column>
+        </template>
+      </b-table>
+    </section>
   </div>
 </template>
 
@@ -51,14 +65,25 @@
           page: 1,
           per:  CONST.PER,
         },
+        search: {
+          target: 'mine',
+          keyword: '',
+        },
         isLoading: false
+      }
+    },
+    watch: {
+      "search.target": function() {
+        this.pager.page = 1
+        this.fetch()
       }
     },
     methods: {
       fetch: function() {
         const params = {
           page: this.pager.page,
-          per:  this.pager.per
+          per:  this.pager.per,
+          members: this.search.target == 'mine' ? [11] : null // TODO: ログイン中ユーザに差し替え
         }
         this.isLoading = true
         http.getEvents(params).then((response) => {
