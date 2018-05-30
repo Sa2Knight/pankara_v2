06e175ed52f4c2b095e1fa3e4e8da6fd4a829db9
Shingo Sasaki <shingo.sasaki.0529@gmail.com>
2018-05-30 14:50:17 +0000

    FontAwsomeを導入

--- app/javascript/main.js
+++ app/javascript/main.js
@@ -2,6 +2,8 @@ import Vue        from 'vue'
 import VueRouter  from 'vue-router'
 import Vuetify    from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
+import '@fortawesome/fontawesome-free-webfonts/css/fontawesome.css'
+import '@fortawesome/fontawesome-free-webfonts/css/fa-solid.css'
 
 Vue.use(VueRouter)
 Vue.use(Vuetify)
@@ -13,5 +15,8 @@ const routes = [
 
 // ページ全体をVueコンポーネント化
 const app = new Vue({
+  data: {
+    is_show_navigation: false
+  },
   router: new VueRouter({ routes })
 }).$mount('#app')
