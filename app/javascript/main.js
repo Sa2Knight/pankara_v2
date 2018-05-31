0c47766800d61e255dbed98499b7148c89dc9788
Shingo Sasaki <shingo.sasaki.0529@gmail.com>
2018-05-31 16:38:34 +0000

    eslint導入

--- app/javascript/main.js
+++ app/javascript/main.js
@@ -4,17 +4,18 @@ import Vuetify    from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 import '@fortawesome/fontawesome-free-webfonts/css/fontawesome.css'
 import '@fortawesome/fontawesome-free-webfonts/css/fa-solid.css'
+import PageEvents from './component/page/PageEvents'
 
 Vue.use(VueRouter)
 Vue.use(Vuetify)
 
 // URLに対応するコンポーネントを定義
 const routes = [
-  { path: '/', component: require('./component/page/PageEvents').default }
+  { path: '/', component: PageEvents }
 ]
 
 // ページ全体をVueコンポーネント化
-const app = new Vue({
+new Vue({
   data: {
     is_show_navigation: false
   },
