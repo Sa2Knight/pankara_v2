24e3a114ff5cefe618dc22be7720cb9c0598f76b
Shingo Sasaki <shingo.sasaki.0529@gmail.com>
2018-06-21 14:50:38 +0000

    カラオケ詳細画面の歌唱履歴のサブメニューを実装

--- app/javascript/component/page/event/HistoryCard.vue
+++ app/javascript/component/page/event/HistoryCard.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="history-card" v-bind:class="{ selected: isSelected, 'elevation-10': isSelected }">
-    <v-container grid-list-md text-xs-center>
+  <div class="history-card" v-bind:class="{ selected: isSelected, 'elevation-12': isSelected }">
+    <v-container @click="select" grid-list-md text-xs-center>
       <v-layout row>
         <v-flex xs4>
           <v-song-thumbnail :song="history.song" />
         </v-flex>
-        <v-flex xs7 @click="select">
+        <v-flex xs7>
           <p class="song-name collapse">{{ history.song.name }}</p>
           <p class="artist-name collapse">{{ history.song.artist.name }}</p>
           <span>キー: {{ history.key }}</span>
@@ -17,10 +17,31 @@
         </v-flex>
       </v-layout>
       <v-layout row justify-space-around v-show="isSelected">
-        <v-icon>fas fa-clipboard-list</v-icon>
-        <v-icon>fas fa-pencil-alt</v-icon>
-        <v-icon>fas fa-trash-alt</v-icon>
-        <v-icon>fas fa-ellipsis-h</v-icon>
+        <v-btn icon>
+          <v-icon>fas fa-clipboard-list</v-icon>
+        </v-btn>
+        <v-btn icon>
+          <v-icon>fas fa-pencil-alt</v-icon>
+        </v-btn>
+        <v-btn icon>
+          <v-icon>fas fa-trash-alt</v-icon>
+        </v-btn>
+        <v-menu bottom left>
+          <v-btn slot="activator" icon>
+            <v-icon>fas fa-ellipsis-h</v-icon>
+          </v-btn>
+          <v-list>
+            <v-list-tile>
+              <v-list-tile-title>動画再生</v-list-tile-title>
+            </v-list-tile>
+            <v-list-tile>
+              <v-list-tile-title>楽曲詳細</v-list-tile-title>
+            </v-list-tile>
+            <v-list-tile>
+              <v-list-tile-title>歌手詳細</v-list-tile-title>
+            </v-list-tile>
+          </v-list>
+        </v-menu>
       </v-layout>
     </v-container>
   </div>
