49ccdd176400ed00043d2051455c9e83ba621f88
Shingo Sasaki <shingo.sasaki.0529@gmail.com>
2018-06-21 15:03:10 +0000

    カラオケ詳細画面の画面下部に追加ボタンを設置

--- app/javascript/component/page/event/PageEvent.vue
+++ app/javascript/component/page/event/PageEvent.vue
@@ -2,12 +2,20 @@
   <div>
     <div v-if="event">
       <h1>歌唱履歴</h1>
-        <v-layout row wrap>
-          <v-flex xs12 sm6 md6 lg4 v-for="(history, i) in event.histories">
-            <history-card :history="history" />
-            <v-divider />
-          </v-flex>
-        </v-layout>
+      <!-- 歌唱履歴一覧 -->
+      <v-layout row wrap class="pb-5">
+        <v-flex xs12 sm6 md6 lg4 v-for="(history, i) in event.histories">
+          <history-card :history="history" />
+          <v-divider />
+        </v-flex>
+      </v-layout>
+
+      <!-- 追加ボタン -->
+      <v-fab-transition>
+        <v-btn color="pink" class="elevation-20" fixed bottom right fab dark>
+          <v-icon>add</v-icon>
+        </v-btn>
+      </v-fab-transition>
     </div>
   </div>
 </template>
