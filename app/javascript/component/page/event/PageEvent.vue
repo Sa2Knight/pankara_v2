84abec252b6fcccc9494ecb9292616025287c143
Shingo Sasaki <shingo.sasaki.0529@gmail.com>
2018-06-26 14:25:19 +0000

    front: カラオケ詳細画面にダブメニューを借導入

--- app/javascript/component/page/event/PageEvent.vue
+++ app/javascript/component/page/event/PageEvent.vue
@@ -1,14 +1,42 @@
 <template>
   <div>
     <div v-if="event">
-      <h1>歌唱履歴</h1>
-      <!-- 歌唱履歴一覧 -->
-      <v-layout row wrap class="pb-5">
-        <v-flex xs12 sm6 md6 lg4 v-for="(history, i) in event.histories">
-          <history-card :history="history" />
-          <v-divider />
-        </v-flex>
-      </v-layout>
+      <v-tabs icons-and-text fixed-tabs centered dark color="cyan">
+        <!-- タブメニュー -->
+        <v-tabs-slider color="yellow"></v-tabs-slider>
+        <v-tab href="#overview">
+          概要
+          <v-icon>fas fa-clipboard-list</v-icon>
+        </v-tab>
+        <v-tab href="#members">
+          メンバー
+          <v-icon>fas fa-users</v-icon>
+        </v-tab>
+        <v-tab href="#histories">
+          歌唱履歴
+          <v-icon>fas fa-list-ol</v-icon>
+        </v-tab>
+
+        <!-- 概要 -->
+        <v-tab-item id="overview">
+          概要
+        </v-tab-item>
+
+        <!-- メンバー -->
+        <v-tab-item id="members">
+          メンバー
+        </v-tab-item>
+
+        <!-- 歌唱履歴一覧 -->
+        <v-tab-item id="histories">
+          <v-layout row wrap class="pb-5">
+            <v-flex xs12 sm6 md6 lg4 v-for="(history, i) in event.histories">
+              <history-card :history="history" />
+              <v-divider />
+            </v-flex>
+          </v-layout>
+        </v-tab-item>
+      </v-tabs>
 
       <!-- 追加ボタン -->
       <v-fab-transition>
