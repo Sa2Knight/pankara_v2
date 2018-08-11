24dd9ea48b9397206bf95f14e5d371cd46a6c885
Shingo Sasaki <shingo.sasaki.0529@gmail.com>
2018-08-11 08:45:00 +0000

    front: v-boxコンポーネントを作って差し替え

---
+++ app/javascript/component/common/VBox.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="v-box">
+    <div class="label">
+      {{ label }}
+    </div>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.v-box {
+  border: 1px solid;
+  border-color: #ddd;
+  .label {
+    padding: 10px;
+    background-color: #eee;
+    text-align: center;
+  }
+}
+</style>
+
+<script>
+  export default {
+    props: {
+      label: {
+        type: String,
+        required: true
+      }
+    }
+  }
+</script>
--- app/javascript/component/page/event/EventDetail.vue
+++ app/javascript/component/page/event/EventDetail.vue
@@ -1,57 +1,16 @@
 <template>
   <div class="event-detail">
-    <div class="box">
-      <div class="label center">
-        <span>参加メンバー</span>
-      </div>
-      <div class="members center">
-        <event-members />
-      </div>
-    </div>
+    <v-box label="カラオケ情報">ここにカラオケ情報</v-box>
+    <v-box label="参加メンバー"><event-members /></v-box>
   </div>
 </template>
 
-<style lang="scss">
-.event-detail {
-  .box {
-    border: 1px solid;
-    border-color: #ddd;
-    .label {
-      padding: 10px;
-      background-color: #eee;
-    }
-    .members {
-      margin-top: 5px;
-      margin-bottom: 5px;
-    }
-  }
-}
-</style>
-
 <script>
-  import CONST from '../../../lib/constants'
   import { mapState } from 'vuex'
   export default {
-    data: function() {
-      return {
-        title: '2018/03/03のカラオケ',
-        date: '',
-        isShowDatePickerModal: false,
-        creator: { // DUMMY
-          image_url: 'user_icon/sa2knight.png'
-        }
-      }
-    },
-    computed: {
-      users: function() {
-        return this.event.members.map((member) => member.user)
-      },
-      ...mapState({
-        event: store => store.event.event
-      })
-    },
     components: {
-      EventMembers: require('./EventMembers').default
+      EventMembers: require('./EventMembers').default,
+      VBox: require('../../common/VBox').default,
     }
   }
 </script>
