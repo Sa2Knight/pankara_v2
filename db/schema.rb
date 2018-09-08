3065e48ee3c27f19c053b3bc7fdcd427c1bc8733
Shingo Sasaki <shingo.sasaki.0529@gmail.com>
2018-09-08 16:31:33 +0000

    Artistテーブルにurl列を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_09_135000) do
+ActiveRecord::Schema.define(version: 2018_09_08_162419) do
 
   create_table "artists", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.text "description"
+    t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
