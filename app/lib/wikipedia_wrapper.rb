7d37dcab5ba6ae0dfdc2b54bef1f65b0bf5192b6
Shingo Sasaki <shingo.sasaki.0529@gmail.com>
2018-09-08 21:49:32 +0000

    Wikipediaからサマリーをフェッチする際に、ページの有無に応じてURLも戻すように修正

--- app/lib/wikipedia_wrapper.rb
+++ app/lib/wikipedia_wrapper.rb
@@ -5,6 +5,10 @@ module WikipediaWrapper
   # 指定したタイトルで記事を検索し、そのサマリーを戻す
   #
   def fetch_sammary(title:)
-    Wikipedia.find(title).summary.presence
+    page = Wikipedia.find(title)
+    {
+      summary: page.summary.presence,
+      url: page.content.present? ? page.fullurl : nil
+    }
   end
 end
