11b4a358bad03be54f0831779bc8d8fa80729360
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2018-09-16 08:58:13 +0000

    歌手詳細APIのテストコードを実験的に改変

--- spec/requests/artists/show_spec.rb
+++ spec/requests/artists/show_spec.rb
@@ -6,22 +6,35 @@ RSpec.describe 'artists#show', type: :request do
   let(:artist)       { FactoryBot.create(:artist) }
   let(:artist_id)    { artist.id }
 
-  before do
+  subject do
     request(:get, "/api/artists/#{artist_id}", user: current_user)
+    JSON.parse(response.body).symbolize_keys
   end
 
   describe '正常系' do
+    describe 'レスポンス構成の検証' do
+      it do
+        should include(
+          :id,
+          :name,
+          :description,
+          :url
+        )
+      end
+    end
     describe '基本情報の検証' do
       it do
-        expect(body['id']).to eq artist.id
-        expect(body['name']).to eq artist.name
-        expect(body['description']).to eq artist.description
-        expect(body['url']).to eq artist.url
+        expect(subject[:id]).to eq artist.id
+        expect(subject[:name]).to eq artist.name
+        expect(subject[:description]).to eq artist.description
+        expect(subject[:url]).to eq artist.url
       end
     end
   end
 
   describe '異常系' do
+    before { subject }
+
     context '歌手が存在しない場合' do
       let(:artist_id) { 0 }
       it_behaves_like '404'
