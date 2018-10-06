ac3f67a678f6327a1c4e1bd6d67fc1ee64bef5ae
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2018-10-06 05:43:06 +0000

    UserEvent#comment削除　フロント対応

--- app/javascript/component/page/event/parts/VMemberCard.vue
+++ app/javascript/component/page/event/parts/VMemberCard.vue
@@ -8,22 +8,9 @@
         </v-flex>
         <v-flex xs9>
           <div class="member-info arrow-box right">
-            <div v-if="isShowComment">{{ comment }}</div>
-            <div v-else>
-              <div>歌唱数: {{ member.history_size }}</div>
-              <div>最高点: {{ member.max_score}}</div>
-              <div>平均点: {{ member.average_score}}</div>
-            </div>
-            <div class="buttons">
-              <v-btn icon v-show="isShowComment" @click="">
-                <v-icon>fas fa-pencil-alt</v-icon>
-              </v-btn>
-              <!-- 集計情報トグルは一旦非表示にしておく
-              <v-btn icon @click="swipeInfo">
-                <v-icon>fas fa-exchange-alt</v-icon>
-              </v-btn>
-              -->
-            </div>
+            <div>歌唱数: {{ member.history_size }}</div>
+            <div>最高点: {{ member.max_score}}</div>
+            <div>平均点: {{ member.average_score}}</div>
           </div>
         </v-flex>
       </v-layout>
@@ -65,20 +52,10 @@
       }
     },
     props: {
-      // comment, user{id, display_name, image_url}
+      // user{id, display_name, image_url}
       member: {
         type: Object,
         required: true
-      },
-    },
-    computed: {
-      comment: function() {
-        return this.member.comment || '未入力'
-      }
-    },
-    methods: {
-      swipeInfo: function() {
-        this.isShowComment = !this.isShowComment
       },
     },
     components: {
