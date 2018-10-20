0fc1e76b058ca4b43c81a773be3fd5253ff26d54
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2018-10-20 14:30:56 +0000

    アーティスト詳細画面にwikipediaから引用した説明文を表示

--- app/javascript/component/page/artist/PageArtistOverview.vue
+++ app/javascript/component/page/artist/PageArtistOverview.vue
@@ -2,6 +2,12 @@
   <div class="page-artist-overview">
     <template v-if="artist">
       <v-box label="基本情報" />
+        <p class="description">
+          {{ description }}
+          <a v-if="wikipediaUrl" :href="wikipediaUrl">
+            (wikipedia引用)
+          </a>
+        </p>
       <v-box label="楽曲一覧">
         <v-layout row wrap class="pb-5">
           <v-flex xs12 sm6 md4 lg3 v-for="song in artist.songs">
@@ -14,6 +20,12 @@
   </div>
 </template>
 
+<style lang="scss" scoped>
+.description {
+  padding: 5px;
+}
+</style>
+
 <script>
   import { mapState } from 'vuex'
   const namespace = 'artist'
@@ -22,7 +34,17 @@
     computed: {
       ...mapState(namespace, {
         artist: store => store.artist,
-      })
+      }),
+      description: function() {
+        if (this.artist.description) {
+          return `${this.artist.description}`
+        } else {
+          return `"${this.artist.name}" に関する情報がありません`
+        }
+      },
+      wikipediaUrl: function() {
+        return this.artist.description ? this.artist.url : null;
+      }
     },
     components: {
       VSongCard: require('../../common/VSongCard').default,
