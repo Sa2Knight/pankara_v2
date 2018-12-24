8f7f31922a45a07e202e01e3bdbf0c02e0a0a4e2
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2018-12-24 16:42:23 +0000

    close #43 APIでのバリデーションをモデルに委譲

---
+++ app/models/validators/event_validators.rb
@@ -0,0 +1,27 @@
+class EventValidators < ActiveModel::Validator
+  def validate(record)
+    @record = record
+
+    if datetime_blank?
+      record.errors[:datetime] << '日付は必須です'
+    elsif datetime_invalid?
+      record.errors[:datetime] << '日付が不正な文字列です'
+    elsif datetime_future?
+      record.errors[:datetime] << '未来の日付は設定できません'
+    end
+  end
+
+  private
+
+  def datetime_blank?
+    @record.datetime.blank?
+  end
+
+  def datetime_invalid?
+    Date.valid_by?(@record.datetime.to_s).blank?
+  end
+
+  def datetime_future?
+    Date.parse(@record.datetime.to_s).future?
+  end
+end
