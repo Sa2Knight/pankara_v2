df0c446fb5e6e66c63f660b0d959553f71e0434b
笹木 信吾 <shingo.sasaki.0529@gmail.com>
2018-12-26 00:18:15 +0900

    issue #38 CSRF対策 (#47)
    * Validatorのクラス名を修正
    
    * close #38 CSRF対策を実施

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   include Knock::Authenticable
 
-  # TODO: 一時的な対応なので必ずお治す
-  protect_from_forgery with: :null_session
+  protect_from_forgery with: :exception
 end
