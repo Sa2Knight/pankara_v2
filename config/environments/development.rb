6b1cf058512357f50408b79afb0728e798a2bec7
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2018-12-27 20:11:28 +0000

    configファイルを読み進めながら整形

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,33 +1,27 @@
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
 
-  # In the development environment your application's code is reloaded on
-  # every request. This slows down response time but is perfect for development
-  # since you don't have to restart the web server when you make code changes.
+  # ソースに変更を加えた場合次回リクエスト時にリロードする
   config.cache_classes = false
 
-  # Do not eager load code on boot.
+  # 全ての名前空間は事前に読み込まずに必要になった段階で読み込む
   config.eager_load = false
 
-  # Show full error reports.
+  # Railsのエラーをレスポンスに含む
   config.consider_all_requests_local = true
 
-  # Enable/disable caching. By default caching is disabled.
-  # Run rails dev:cache to toggle caching.
+  # 開発環境では rails dev:cache コマンドでキャッシュを使うかを切り替える
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
     config.action_controller.perform_caching = true
-
     config.cache_store = :memory_store
     config.public_file_server.headers = {
       'Cache-Control' => "public, max-age=#{2.days.to_i}"
     }
   else
     config.action_controller.perform_caching = false
-
     config.cache_store = :null_store
   end
 
-  # Store uploaded files on the local file system (see config/storage.yml for options)
+  # ActiveStorageはクラウドでなくローカルを使う
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,59 +1,36 @@
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
 
-  # Code is not reloaded between requests.
+  # ソースに変更を加えてもリロードしない
   config.cache_classes = true
 
-  # Eager load code on boot. This eager loads most of Rails and
-  # your application in memory, allowing both threaded web servers
-  # and those relying on copy on write to perform better.
-  # Rake tasks automatically ignore this option for performance.
+  # 全ての名前空間を事前に読み込む
   config.eager_load = true
 
-  # Full error reports are disabled and caching is turned on.
+  # Railsのエラーをレスポンスに含まない
   config.consider_all_requests_local       = false
+
+  # コンテンツのキャッシュを利用する
   config.action_controller.perform_caching = true
 
-  # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
-  # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
-  # config.require_master_key = true
-
-  # Disable serving static files from the `/public` folder by default since
-  # Apache or NGINX already handles this.
+  # Webサーバ(Nginx,Apachなど)を使わずにAssetsファイルを配信させるか
+  # TODO: どのように配信するか要検討
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
-  # Compress JavaScripts and CSS.
+  # JSの圧縮ライブラリにはuglifierを使用
   config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
 
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
+  # アセットのコンパイルは行わない
+  # TODO: 要検討
   config.assets.compile = false
 
-  # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
-
-  # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = 'http://assets.example.com'
-
-  # Specifies the header that your server uses for sending files.
-  # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
-
-  # Store uploaded files on the local file system (see config/storage.yml for options)
+  # ActiveStorageはクラウドでなくローカルを使う
   config.active_storage.service = :local
 
-  # Mount Action Cable outside main process or domain
-  # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  # ログは警告以上のみ表示
+  # REVIEW: デバッグ情報本当にいらない？
+  config.log_level = :warn
 
-  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
-
-  # Use the lowest log level to ensure availability of diagnostic information
-  # when problems arise.
-  config.log_level = :debug
-
-  # Prepend all log lines with the following tags.
+  # ログにrequest_idを含むようにする
   config.log_tags = [ :request_id ]
 
   # Use a different cache store in production.
