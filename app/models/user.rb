493576710d249b5a48cd36fe8b77eb0fda2cbdfa
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2018-12-29 14:19:41 +0000

    不要なコードの削除とrubocop指摘修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
-  has_many :user_events
+  has_many :user_events, dependent: :destroy
   has_many :events, through: :user_events
   has_many :own_events, class_name: 'Event', dependent: :nullify
-
-  def self.from_token_request(request)
-    User.first
-  end
 
   #
   # 歌唱履歴一覧
