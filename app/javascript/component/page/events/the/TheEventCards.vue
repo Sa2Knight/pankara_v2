f16f511715a9350d9c4c252eaca406178541e677
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2019-01-19 12:53:52 +0000

    カラオケ一覧画面のコンポーネントをリファクタリング

--- app/javascript/component/page/events/the/TheEventCards.vue
+++ app/javascript/component/page/events/the/TheEventCards.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="the-event-cards" v-show="events">
-    <v-pagination-wrapper
+    <VPaginationWrapper
       :pageOrigin="pageOrigin"
       :totalPages="totalPages"
       :changePage="fetchWithPaging"
@@ -9,29 +9,34 @@
                         row wrap disable-initial-sort hide-actions>
         <v-flex slot="item" slot-scope="props" xs12 sm6 md4 lg3 xl2 justify-space-between>
           <a :href="getEventPath(props.item)">
-            <v-event-card :event="props.item" />
+            <VEventCard :event="props.item" />
           </a>
         </v-flex>
       </v-data-iterator>
-    </v-pagination-wrapper>
+    </VPaginationWrapper >
   </div>
 </template>
 
 <script>
   import { mapState, mapActions } from 'vuex'
-  import { ROUTES } from '../../../../lib/routes'
-  const namespace = 'events'
+  import { ROUTES } from '@lib/routes'
+  import VPaginationWrapper from '@component/common/VPaginationWrapper'
+  import VEventCard from '@component/common/VEventCard'
 
   export default {
+    components: {
+      VPaginationWrapper,
+      VEventCard
+    },
     computed: {
-      ...mapState(namespace, {
+      ...mapState('events', {
         events: store => store.events,
         pageOrigin: store => store.pager.page,
         totalPages: store => store.pager.totalPages,
       })
     },
     methods: {
-      ...mapActions(namespace, [
+      ...mapActions('events', [
         'changePage',
         'fetchEvents',
       ]),
@@ -48,9 +53,5 @@
         this.fetchEvents()
       }
     },
-    components: {
-      VPaginationWrapper: require('../../../common/VPaginationWrapper').default,
-      VEventCard: require('./VEventCard').default
-    }
   }
 </script>
