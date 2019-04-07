9aeeff89558af5342e51dc6f124ea21658b9570b
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2019-04-07 15:25:04 +0000

    カラオケカードのデザインを微修正

--- app/javascript/component/common/VEventCard.vue
+++ app/javascript/component/common/VEventCard.vue
@@ -1,16 +1,11 @@
 <template>
   <v-card hover class="mb-2">
-    <v-card-title primary-title>
+    <v-card-title primary-title class="header">
       <h3 class="collapse">{{ event.title }}</h3>
+      <VUserIcons :users="event.members" />
     </v-card-title>
     <v-divider />
     <v-list>
-      <v-list-tile>
-        <v-list-tile-content>メンバー</v-list-tile-content>
-        <v-list-tile-content class="align-end">
-          <VUserIcons :users="event.members" />
-        </v-list-tile-content>
-      </v-list-tile>
       <v-list-tile>
         <v-list-tile-content>日付</v-list-tile-content>
         <v-list-tile-content class="align-end">{{ event.date }}</v-list-tile-content>
@@ -22,6 +17,13 @@
     </v-list>
   </v-card>
 </template>
+
+<style lang="scss" scoped>
+.header {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
 
 <script>
   import VUserIcons from '@component/common/VUserIcons'
